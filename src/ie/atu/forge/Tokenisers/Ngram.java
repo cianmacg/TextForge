4ec43d2da24daa01319d3afb585e56cdfe833fea
cianmacg <scian94@gmail.com>
2025-06-13 21:53:58 +0100

    Completed Shingle tokens.

--- src/ie/atu/forge/Tokenisers/Ngram.java
+++ src/ie/atu/forge/Tokenisers/Ngram.java
@@ -1,12 +1,14 @@
 package ie.atu.forge.Tokenisers;
 
+// Character level N-gram tokenisation. For word N-grams, use Shingle.
 public class Ngram {
     public static String[] tokenise(String input, int window) {
         String[] tokens = new String[input.length() - window + 1];
 
         int i = 0;
-        while (i < input.length() - window) {
-            tokens[i++] = input.substring(i, i + window);
+        while (i < input.length() - (window - 1)) {
+            tokens[i] = input.substring(i, i + window);
+            i++;
         }
 
         return tokens;
--- src/ie/atu/forge/Tokenisers/Shingle.java
+++ src/ie/atu/forge/Tokenisers/Shingle.java
@@ -1,12 +1,27 @@
 package ie.atu.forge.Tokenisers;
 
+// Word level N-gram tokenisation. For character N-grams, use Ngram.
 public class Shingle {
-    public String[] tokenise(String input, int window) {
+    public static String[] tokenise(String input, int window) {
         String[] words = input.split("\\s+");
-        String[] tokens = new String[input.length() - window + 1];
+        String[] tokens = new String[words.length - window + 1];
 
+        if(words.length <= window) {
+            return tokens;
+        }
 
+        int i = 0;
+        while(i < words.length - window + 1) {
+            StringBuilder sb = new StringBuilder();
 
+            for(int j = 0; j < (window - 1); j++) {
+                sb.append(words[i+j]);
+                sb.append(" ");
+            }
+            sb.append(words[i+window - 1]);
+            tokens[i] = sb.toString();
+            i++;
+        }
         return tokens;
     }
 }
