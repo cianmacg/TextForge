7b06fecdcfa31b948c66af21ffb72bd8363cec85
cianmacg <scian94@gmail.com>
2025-06-28 13:11:19 +0100

    Begun implementation of Lovin.java.

--- src/ie/atu/forge/Stemmers/Lovin.java
+++ src/ie/atu/forge/Stemmers/Lovin.java
@@ -1,4 +1,120 @@
 package ie.atu.forge.Stemmers;
 
 public class Lovin {
+
+    private boolean condition_a(char[] input) {
+        return true;
+    }
+
+    private boolean condition_b(char[] input) {
+        return input.length >= 3;
+    }
+
+    private boolean condition_c(char[] input) {
+        return input.length >= 4;
+    }
+
+    private boolean condition_d(char[] input) {
+        return input.length >= 5;
+    }
+
+    private boolean condition_e(char[] input) {
+        return !(input[input.length - 1] == 'e');
+    }
+
+    private boolean condition_f(char[] input) {
+        return input.length >= 3 && !(input[input.length - 1] == 'e');
+    }
+
+    private boolean condition_g(char[] input) {
+        return input.length >= 3 && (input[input.length - 1] == 'f');
+    }
+
+    private boolean condition_h(char[] input) {
+        return false;
+    }
+
+    private boolean condition_i(char[] input) {
+        return false;
+    }
+
+    private boolean condition_j(char[] input) {
+        return false;
+    }
+
+    private boolean condition_k(char[] input) {
+        return false;
+    }
+
+    private boolean condition_l(char[] input) {
+        return false;
+    }
+
+    private boolean condition_m(char[] input) {
+        return false;
+    }
+
+    private boolean condition_n(char[] input) {
+        return false;
+    }
+
+    private boolean condition_o(char[] input) {
+        return false;
+    }
+
+    private boolean condition_p(char[] input) {
+        return false;
+    }
+
+    private boolean condition_q(char[] input) {
+        return false;
+    }
+
+    private boolean condition_r(char[] input) {
+        return false;
+    }
+
+    private boolean condition_s(char[] input) {
+        return false;
+    }
+
+    private boolean condition_t(char[] input) {
+        return false;
+    }
+
+    private boolean condition_u(char[] input) {
+        return false;
+    }
+
+    private boolean condition_v(char[] input) {
+        return false;
+    }
+
+    private boolean condition_w(char[] input) {
+        return false;
+    }
+
+    private boolean condition_x(char[] input) {
+        return false;
+    }
+
+    private boolean condition_y(char[] input) {
+        return false;
+    }
+
+    private boolean condition_z(char[] input) {
+        return false;
+    }
+
+    private boolean condition_aa(char[] input) {
+        return false;
+    }
+
+    private boolean condition_bb(char[] input) {
+        return false;
+    }
+
+    private boolean condition_cc(char[] input) {
+        return false;
+    }
 }
