f44a38642129512e296a268b9ffbefe3f762dd23
cianmacg <scian94@gmail.com>
2025-07-04 10:17:02 +0100

    Added some simple test files for 'Lovin.java' and 'Porter.java'.

--- src/main.java
+++ src/main.java
@@ -3,48 +3,13 @@ import ie.atu.forge.Stemmers.Porter;
 
 public class main {
     public static void main(String[] args) {
-/*        String[] input = {
-                "rubbing",
-                "believe",
-                "induction",
-                "comsumption",
-                "absorption",
-                "recursive",
-                "administrate",
-                "parametric",
-                "dissolved",
-                "angular",
-                "vibex",
-                "index",
-                "apex",
-                "cortex",
-                "anthrax",
-                "matrix",
-                "persuade",
-                "evade",
-                "decide",
-                "elide",
-                "deride",
-                "expand",
-                "defend",
-                "respond",
-                "collude",
-                "obtrude",
-                "adhere",
-                "remit",
-                "extent",
-                "converted",
-                "parenthetic",
-                "analytic",
-                "analyzed"
-        };*/
         String[] input = {
-                "movement"
+                "different"
         };
 
 /*        String stem = Lovin.stem("rubbing");
         System.out.println(stem);*/
-        String[] stems = Lovin.stem(input);
+        String[] stems = Porter.stem(input);
 
         for(String stem : stems) {
             System.out.println(stem);
