fa7d4ac9ac6c618afb81ed778f831f21681b2b00
cianmacg <scian94@gmail.com>
2025-07-14 13:33:22 +0100

    Updated Cosine to overload 'similarity' method (maps or int[]s are now accepted).

--- src/ie/atu/forge/Similarity/AlignmentFree/Cosine.java
+++ src/ie/atu/forge/Similarity/AlignmentFree/Cosine.java
@@ -3,21 +3,16 @@ package ie.atu.forge.Similarity.AlignmentFree;
 import java.util.*;
 
 public class Cosine {
-    /*
-    Unsure what I should be taking in here. A mapping of tokens to count of each text is a good starting point.
-    I can easily create a vector representation from this, and the user can determine what the tokens are (words, ngrams, etc).
-     */
-    public static double similarity(Map<String, Integer> s1, Map<String, Integer> s2) {
-        Set<String> vocabulary = new HashSet<>(s1.keySet());
-        vocabulary.addAll(s2.keySet());
 
-        double dotProd = 0.0d;
-        double mag1 = 0.0d;
-        double mag2 = 0.0d;
+    // Overloading similarity function to also allow uses to pass a pre-made count vector
+    public static double similarity(int[] v1, int[] v2) {
+        double dotProd = 0.0d, mag1 = 0.0d, mag2 = 0.0d;
 
-        for(String token: vocabulary) {
-            int d1 = s1.getOrDefault(token, 0);
-            int d2 = s2.getOrDefault(token, 0);
+        // Vectors need to be the same length.
+        if(v1.length != v2.length) return -1.0d;
+
+        for(int i = 0; i < v1.length; i++) {
+            int d1 = v1[i], d2 = v2[i];
 
             dotProd += d1 * d2;
             mag1 += d1 * d1;
@@ -25,6 +20,26 @@ public class Cosine {
         }
 
         return dotProd / Math.sqrt(mag1 * mag2);
+    }
+
+    public static double similarity(Map<String, Integer> s1, Map<String, Integer> s2) {
+        Set<String> vocabulary = new HashSet<>(s1.keySet());
+        vocabulary.addAll(s2.keySet());
+
+        int[] v1 = new int[vocabulary.size()], v2 = new int[vocabulary.size()];
+
+        int i = 0;
+        // Populate the count vectors.
+        for(String token: vocabulary) {
+            v1[i] = s1.getOrDefault(token, 0);
+            v2[i] = s2.getOrDefault(token, 0);
+        }
+
+        return similarity(v1, v2);
+    }
+
+    public static double distance(int[] v1, int[] v2) {
+        return 1 - similarity(v1, v2);
     }
 
     public static double distance(Map<String, Integer> b1, Map<String, Integer> b2) {
