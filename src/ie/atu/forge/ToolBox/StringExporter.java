4a5dc1e1c05f7abe39d5aae0a0730e1bd2fe29a3
cianmacg <scian94@gmail.com>
2025-07-28 22:37:33 +0100

    Added StringExporter to ToolBox. This just outputs a string or string array to a file called 'Output.txt'.

---
+++ src/ie/atu/forge/ToolBox/StringExporter.java
@@ -0,0 +1,36 @@
+package ie.atu.forge.ToolBox;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class StringExporter {
+    public static void toFile(String output, String path) throws IOException {
+        if(output.isEmpty()) return;
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path + "Output.txt"));
+        writer.write(output);
+    }
+
+    public static void toFile(String[] output, String path) throws IOException {
+        if(output.length == 0) return;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(output[0]);
+
+        for(int i = 1; i < output.length; i++) {
+            sb.append(" ");
+            sb.append(output[i]);
+        }
+
+        toFile(sb.toString(), path);
+    }
+
+    public static void toFile(String output) throws IOException {
+        toFile(output, "");
+    }
+
+    public static void toFile(String[] output) throws IOException {
+        toFile(output, "");
+    }
+}
